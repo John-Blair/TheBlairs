6528e0dbd80bbde2b713fa662a5df3237aedfd5a
John <john_blair@hotmail.com>
2017-07-03 16:25:43 +0100

    Close mobile menu after click.

--- TheBlairs/Scripts/theblairs.js
+++ TheBlairs/Scripts/theblairs.js
@@ -15,3 +15,12 @@ $(function () {
 
 
 });
+
+
+
+
+$(document).on('click', '.navbar-collapse.in', function (e) {
+    if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+        $(this).collapse('hide');
+    }
+});
